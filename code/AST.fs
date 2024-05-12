9fd42f2805be0fcd4dd93d73c72173ee479e6f85
Michael Faulkner <mfaulkner1155@gmail.com>
2024-05-12 01:42:41 -0400

    More tests

--- code/AST.fs
+++
@@ -1,9 +0,0 @@
-module AST
-
-type Expression =
-| Number of double
-| Variable of char
-| Addition of Expression list
-| Multiplication of Expression list
-| Exponentiation of Expression * Expression
-| Sequence of Expression list
